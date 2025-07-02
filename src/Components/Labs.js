9841e4f8eb83484251a5056fe10ee0c06d7d2a49
Raja <kraj9380286@gmail.com>
2025-07-02 10:27:53 +0530

    commited

--- src/Components/Labs.js
+++ src/Components/Labs.js
@@ -1,9 +1,19 @@
 import React from 'react'
+import { Navigate, useNavigate } from 'react-router-dom'
 
 const Labs = () => {
+  const Navigate=useNavigate();
+  function ClickHandler(){
+    Navigate('/about');
+  }
   return (
     <div>
+    <div>
       This is labs page
+    </div>
+     <div>
+      <button onClick={ClickHandler}>Move to About page</button>
+    </div>
     </div>
   )
 }
